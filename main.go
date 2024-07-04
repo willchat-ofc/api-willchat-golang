000c05d0b08f44cb0d40a74a1af6e5e21f0ff353
willian <willian.cacoelho@gmail.com>
2024-07-03 22:35:40 -0300

    feat: set up server

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
 
+	"github.com/willchat-ofc/api-willchat-golang/internal/setup"
+	"github.com/willchat-ofc/api-willchat-golang/internal/setup/config"
+)
+
+func main() {
+	port := ":8080"
+	config.LoadEnvFile(".env")
+
+	fmt.Println("server is running with port", port)
+
+	err := http.ListenAndServe(port, setup.Server())
+
+	if err != nil {
+		fmt.Println("error ocurred: ", err.Error())
+	}
 }
