6c20e97022fe110a04c7071906816ba673b68ad0
willian <willian.cacoelho@gmail.com>
2024-07-03 23:06:56 -0300

    feat(config): set up router config

---
+++ internal/setup/config/router.go
@@ -0,0 +1,11 @@
+package config
+
+import (
+	"net/http"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func SetupRoutes(server *http.ServeMux, db *mongo.Database) {
+
+}
--- internal/setup/server.go
+++ internal/setup/server.go
@@ -2,14 +2,17 @@ package setup
 
 import (
 	"net/http"
+
+	"github.com/willchat-ofc/api-willchat-golang/internal/infra/db/mongodb/helpers"
+	"github.com/willchat-ofc/api-willchat-golang/internal/setup/config"
 )
 
 func Server() *http.ServeMux {
 	mux := http.NewServeMux()
 
-	//db := helpers.MongoHelper()
+	db := helpers.MongoHelper()
 
-	//config.SetupRoutes(mux, db)
+	config.SetupRoutes(mux, db)
 
 	return mux
 }
