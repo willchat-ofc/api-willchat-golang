7b0537f23a6011c4506dc879eee398c544e07531
willian <willian.cacoelho@gmail.com>
2024-07-05 23:39:25 -0300

    feat(routes): set up CreateChat router

--- internal/setup/routes/create-chat.go
+++ internal/setup/routes/create-chat.go
@@ -3,9 +3,12 @@ package routes
 import (
 	"net/http"
 
+	"github.com/willchat-ofc/api-willchat-golang/internal/setup/adapters"
+	"github.com/willchat-ofc/api-willchat-golang/internal/setup/factory"
+	"github.com/willchat-ofc/api-willchat-golang/internal/setup/middlewares"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 func CreateChat(server *http.ServeMux, db *mongo.Database) {
-	// server.Handle("/", middlewares.VerifyAccessToken(adapters.AdaptRoute(controllers.NewCreateChatController())))
+	server.Handle("POST /chat", middlewares.VerifyAccessToken(adapters.AdaptRoute(factory.MakeCreateChatController(db))))
 }
