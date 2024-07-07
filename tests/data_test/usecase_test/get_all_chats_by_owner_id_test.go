89e88485674fdda07965c200ee1849af86c4f1fd
willian <willian.cacoelho@gmail.com>
2024-07-07 13:11:18 -0300

    test(usecase): ensure DbGetAllChatsByOwnerId returns a slice of chats if success

---
+++ tests/data_test/usecase_test/get_all_chats_by_owner_id_test.go
@@ -0,0 +1,45 @@
+package usecase_test
+
+import (
+	"testing"
+
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+	"github.com/willchat-ofc/api-willchat-golang/internal/data/protocols"
+	"github.com/willchat-ofc/api-willchat-golang/internal/data/usecase"
+	"github.com/willchat-ofc/api-willchat-golang/tests/mocks"
+)
+
+func setupDbGetAllChatsByOwnerIdMocks(t *testing.T) (*usecase.DbGetAllChatsByOwnerId, *mocks.MockGetAllChatsByOwnerIdRepository, *gomock.Controller) {
+	ctrl := gomock.NewController(t)
+
+	mockGetAllChatsByOwnerIdRepository := mocks.NewMockGetAllChatsByOwnerIdRepository(ctrl)
+	sut := usecase.NewDbGetAllChatsByOwnerId(mockGetAllChatsByOwnerIdRepository)
+
+	return sut, mockGetAllChatsByOwnerIdRepository, ctrl
+}
+
+func TestDbGetAllChatsById(t *testing.T) {
+	t.Run("Success", func(t *testing.T) {
+		sut, getAllChatsByOwnerIdRepository, ctrl := setupDbGetAllChatsByOwnerIdMocks(t)
+		defer ctrl.Finish()
+
+		getAllChatsByOwnerIdResponse := &protocols.GetAllChatsByOwnerIdRepositoryOutput{
+			Id:        "fake-id",
+			CreatedAt: "fake-date",
+			OwnerId:   "fake-owner-id",
+		}
+
+		var getAllChatsByOwnerIdResponseSlice []*protocols.GetAllChatsByOwnerIdRepositoryOutput
+		getAllChatsByOwnerIdResponseSlice = append(getAllChatsByOwnerIdResponseSlice, getAllChatsByOwnerIdResponse)
+		getAllChatsByOwnerIdResponseSlice = append(getAllChatsByOwnerIdResponseSlice, getAllChatsByOwnerIdResponse)
+
+		getAllChatsByOwnerIdRepository.EXPECT().Get("fake-owner-id").Return(getAllChatsByOwnerIdResponseSlice, nil)
+
+		chatData, err := sut.Get("fake-owner-id")
+		require.NoError(t, err)
+		require.Equal(t, chatData[0].Id, "fake-id")
+		require.Equal(t, chatData[0].CreatedAt, "fake-date")
+		require.Equal(t, chatData[0].OwnerId, "fake-owner-id")
+	})
+}
