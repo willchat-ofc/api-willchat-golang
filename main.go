ae7137f99b5075c0a1f1bf5edb340a88b8113e98
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-07-24 15:08:34 -0300

    chore: create a interrupt channel when close the application

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 
 	"github.com/willchat-ofc/api-willchat-golang/internal/setup"
@@ -23,5 +27,20 @@ func main() {
 		WriteTimeout: 1 * time.Second,
 	}
 
-	sm.ListenAndServe()
+	go func() {
+		err := sm.ListenAndServe()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal, 1)
+	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
+
+	sig := <-sigChan
+	log.Println("received terminate, graceful shutdown", sig)
+
+	tc, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+	defer cancel()
+	sm.Shutdown(tc)
 }
