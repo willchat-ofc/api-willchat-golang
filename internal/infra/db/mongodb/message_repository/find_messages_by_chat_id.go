78bb603bc666f075196ce42d9538caca3b813c31
Willian Cavalcanti Coelho <willian.cacoelho@gmail.com>
2024-08-12 17:54:38 -0300

    feat: implement offset and limit in the repository

--- internal/infra/db/mongodb/message_repository/find_messages_by_chat_id.go
+++ internal/infra/db/mongodb/message_repository/find_messages_by_chat_id.go
@@ -3,6 +3,7 @@ package message_repository
 import (
 	"context"
 
+	"github.com/willchat-ofc/api-willchat-golang/internal/data/protocols"
 	"github.com/willchat-ofc/api-willchat-golang/internal/domain/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
@@ -19,14 +20,14 @@ func NewFindMessagesByChatIdMongoRepository(db *mongo.Database) *FindMessagesByC
 	}
 }
 
-func (c *FindMessagesByChatIdMongoRepository) Find(chatId string) ([]*models.Message, error) {
+func (c *FindMessagesByChatIdMongoRepository) Find(data *protocols.FindMessagesByChatIdRepositoryInput) ([]*models.Message, error) {
 	collection := c.Db.Collection("message")
 
-	filter := bson.D{{Key: "chat_id", Value: chatId}}
+	filter := bson.D{{Key: "chat_id", Value: data.ChatId}}
 
 	options := new(options.FindOptions)
-	options.SetSkip(10)
-	options.SetLimit(10)
+	options.SetSkip(int64(data.Offset))
+	options.SetLimit(int64(data.Limit))
 	cursor, err := collection.Find(context.TODO(), filter, options)
 
 	if err != nil {
